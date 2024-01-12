decfa945b86b0b19c170b133c5af95862431d958
Robin <rirobink@gmail.com>
2024-01-12 17:02:00 +0600

    UI modified

--- lib/VIew/NewsContainer.dart
+++ lib/VIew/NewsContainer.dart
@@ -21,16 +21,41 @@ class NewsContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(imgurl),
-          Text(newshead),
-          Text(newsdes),
-          OutlinedButton(onPressed: (){
-            
+
+          Image.network(imgurl,
+            fit: BoxFit.cover,
+            height: 400,
+            width: MediaQuery.of(context).size.width,
+
+          ),
+          SizedBox(height: 5,),
+          Text(newshead,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+          SizedBox(height: 10,),
+          Text(newsdes,style: TextStyle(fontSize: 16),),
+          Spacer(),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                child: OutlinedButton(onPressed: (){
+                 print('Go to this link $newsurl');
 
 
-          }, child: Text("ReadMore"))
+                }, child: Text("ReadMore")),
+              ),
+            ]
+
+          ),
+
 
 
 
